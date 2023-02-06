dd1da4360f5109d40b02f0b72b85abd482626e4b
trimeo806 <trimeo806@gmail.com>
2023-02-06 22:52:49 +0700

    commit to trimeo806

---
+++ src/components/Result.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Result({user1GameItem, user2GameItem, result}) {
+  return (
+    <div>
+      <h1 className="result-header">Result</h1>
+      <div className="result-wrapper">
+        {user1GameItem && <span>{user1GameItem.name}</span>}
+        <span className="result">{result}</span>
+        {user2GameItem && <span>{user2GameItem.name}</span>}
+      </div>
+    </div>
+  );
+}
+
+export default Result;
